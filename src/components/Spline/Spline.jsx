6d4b5209ba4e280868c169083fb448c1b1983f27
Dodo <ztongci@gmail.com>
2023-04-22 01:26:21 +0900

    circle background

--- src/components/Spline/Spline.jsx
+++ src/components/Spline/Spline.jsx
@@ -1,6 +1,7 @@
 const  donate= "https://prod.spline.design/qglvba8PGlcotxnF/scene.splinecode";
 const  pipe= "https://prod.spline.design/oprkBY2tXEsWtRKI/scene.splinecode";
-const scens = [donate,pipe];
+const colorCircle = "https://prod.spline.design/xZdQhuxKWEt4ErTh/scene.splinecode";
+const scens = [donate, pipe, colorCircle];
 
 export const randomBackgroundUrl = ()=>{
     const random = Math.floor(Math.random() * scens.length);
