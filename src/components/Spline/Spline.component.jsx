87bcfe37bc759053290dc5f97f4f081c84195522
Dodo <ztongci@gmail.com>
2023-08-31 16:01:07 +0900

    background reverse

--- src/components/Spline/Spline.component.jsx
+++ src/components/Spline/Spline.component.jsx
@@ -7,8 +7,9 @@ import {randomBackgroundUrl} from "./Spline";
 
 export default function SplineBackround() {
   const UrlBackground = randomBackgroundUrl();
-  console.log(UrlBackground==="https://prod.spline.design/oprkBY2tXEsWtRKI/scene.splinecode");
-  return (
+  return (<div>
+    <div style={{backgroundColor:"rgb(0,0,255,0)", width: "100%", height: "100%", position: "fixed", zIndex: 1}}/>
     <DodoSpline scene={UrlBackground} UrlBackground={UrlBackground==="https://prod.spline.design/oprkBY2tXEsWtRKI/scene.splinecode"}/>
+    </div>
   );
 }
