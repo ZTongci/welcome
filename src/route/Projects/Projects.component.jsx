bc9e5b725c0337afce0912217851f3b079b1e4d4
Dodo <ztongci@gmail.com>
2024-01-18 19:07:27 +0900

    reverse click link crash

--- src/route/Projects/Projects.component.jsx
+++ src/route/Projects/Projects.component.jsx
@@ -1,25 +1,36 @@
 import { Fragment } from "react";
-import {HeroWrap, AllPage, LatestWork, TextWrapper2, Discribe, NavLink,ProductIMG} from "./Projects.styles"
+import {
+  HeroWrap,
+  AllPage,
+  LatestWork,
+  TextWrapper2,
+  Discribe,
+  NavLink,
+  ProductIMG,
+} from "./Projects.styles";
 import { useParams } from "react-router-dom";
-import {projectList} from "./Projects"
+import { projectList } from "./Projects";
 
-const Projects =()=> {
-    const { project } = useParams();
-    const {title,url, demonstration, imgURL} = projectList[project]
-    return (
-        <Fragment>
-            <HeroWrap/ > 
-            <AllPage>
-                <LatestWork>PROJECT SHOWCASE</LatestWork>
-                <TextWrapper2>{title}</TextWrapper2>
-                <Discribe>{demonstration}</Discribe>
-                <NavLink href={url}>visit live site →</NavLink>
-                <ProductIMG  alt="Product-IMG" src={imgURL} />
-                {
-                projectList[project].imgURL2 && <ProductIMG  alt="Product-IMG2" src={projectList[project].imgURL2} />}
-            </AllPage>
-        </Fragment>
-    );
-}
+const Projects = () => {
+  const { project } = useParams();
+  const { title, url, demonstration, imgURL } = projectList[project];
+  return (
+    <Fragment>
+      <HeroWrap />
+      <AllPage>
+        <LatestWork>PROJECT SHOWCASE</LatestWork>
+        <TextWrapper2>{title}</TextWrapper2>
+        <Discribe>{demonstration}</Discribe>
+        <NavLink href={url} target="_blank" rel="noopener noreferrer">
+          visit live site →
+        </NavLink>
+        <ProductIMG alt="Product-IMG" src={imgURL} />
+        {projectList[project].imgURL2 && (
+          <ProductIMG alt="Product-IMG2" src={projectList[project].imgURL2} />
+        )}
+      </AllPage>
+    </Fragment>
+  );
+};
 
 export default Projects;
